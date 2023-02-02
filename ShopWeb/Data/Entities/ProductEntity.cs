92f6ac90385ac162a1beeb49de23740a2e2c3069
novakvova <novakvova@gmail>
2023-02-02 18:41:38 +0200

    Add ProductImages

--- ShopWeb/Data/Entities/ProductEntity.cs
+++ ShopWeb/Data/Entities/ProductEntity.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ShopWeb.Data.Entities
@@ -15,5 +17,6 @@ namespace ShopWeb.Data.Entities
         [ForeignKey("Category")]
         public int CategoryId { get; set; }
         public virtual CategoryEntity Category { get; set; }
+        public virtual ICollection<ProductImageEntity> ProductImages { get; set; }
     }
 }
---
+++ ShopWeb/Migrations/20230202163900_Add tblProductImages.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ShopWeb.Migrations
+{
+    public partial class AddtblProductImages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblProductImages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IsDelete = table.Column<bool>(nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(maxLength: 255, nullable: false),
+                    Priority = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblProductImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tblProductImages_tblProducts_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "tblProducts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblProductImages_ProductId",
+                table: "tblProductImages",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblProductImages");
+        }
+    }
+}
