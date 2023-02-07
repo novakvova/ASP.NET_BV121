fc7979d8958c3918926d26fc6aa7aec48037c633
novakvova <novakvova@gmail>
2023-02-07 19:25:58 +0200

    Begin account controller

--- ShopWeb/Controllers/AccountController.cs
+++ ShopWeb/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShopWeb.Data.Entities.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,11 @@ namespace ShopWeb.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<UserEntity> _userManager;
+        public AccountController(UserManager<UserEntity> userManager)
+        {
+            _userManager= userManager;
+        }
         public IActionResult Login()
         {
             return View();
